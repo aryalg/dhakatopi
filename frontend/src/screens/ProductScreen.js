166a4ed72cee59d9a76adf0a3a209f5450e3a4c9
Bikram Aryal <bikram.aryall@gmail.com>
2021-05-03 17:57:34 +0545

    Added Product Screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,9 @@
 import React from "react";
+import products from "../products";
 
-const ProductScreen = () => {
-  return <div>Product</div>;
+const ProductScreen = ({ match }) => {
+  const product = products.find((p) => p._id === match.params.id);
+  return <div>{product.name}</div>;
 };
 
 export default ProductScreen;
