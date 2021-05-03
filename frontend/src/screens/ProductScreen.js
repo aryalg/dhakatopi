3a808794d2143b28cfe88e4106eb887ef0b90009
Bikram Aryal <bikram.aryall@gmail.com>
2021-05-03 18:00:28 +0545

    Added Image

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Image, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import products from "../products";
 
 const ProductScreen = ({ match }) => {
   const product = products.find((p) => p._id === match.params.id);
-  return <div>{product.name}</div>;
+  return (
+    <>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+
+      <Row md={6}>
+        <Image src={product.image} alt={product.name}></Image>
+      </Row>
+
+      <Row md={3}></Row>
+    </>
+  );
 };
 
 export default ProductScreen;
