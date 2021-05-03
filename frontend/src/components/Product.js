8b55ab769baf59746bc470d4ec7ae6d0ec48be41
Bikram Aryal <bikram.aryall@gmail.com>
2021-05-03 09:13:57 +0545

    display products

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -7,6 +7,22 @@ const Product = ({ product }) => {
       <a href={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
       </a>
+
+      <Card.Body>
+        <a href={`product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+
+        <Card.Text as="div">
+          <div className="my-3">
+            {product.rating} from {product.numReviews} Reviews
+          </div>
+        </Card.Text>
+
+        <Card.Text as="h3">${product.price}</Card.Text>
+      </Card.Body>
     </Card>
   );
 };
