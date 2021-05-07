af51b637e8f0a72ba9ed83153d172fdaed37e0ff
Bikram Aryal <bikram.aryall@gmail.com>
2021-05-07 18:10:40 +0545

    Setting env variables

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const products = require("./data/products");
 const cors = require("cors");
 
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const app = express();
 
 app.use(cors());
@@ -20,4 +24,6 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log("Server is running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
