4deb488637ff0e0677acc1bb09a397d739ac18cf
Bikram Aryal <bikram.aryall@gmail.com>
2021-05-07 18:37:17 +0545

    connecting to database

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,11 @@ import express from "express";
 import products from "./data/products.js";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
