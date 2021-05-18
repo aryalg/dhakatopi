1204fa7ce64625f938263a27d7bc1226ee098330
Bikram Aryal <bikram.aryall@gmail.com>
2021-05-18 18:56:36 +0545

    Custom error handling and middlewares

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,10 +4,35 @@ const router = express.Router();
 
 // We'll have list of user routes
 
-router.get("/login", (req, res) => {
-  res.send({
-    message: "Login Successfully",
-  });
-});
+// Sample Code to Understand Middleware and Error Handling
+
+// User (frontend) request => req -> (MW - next()) -> (MW - next()) -> (MW - next()) -> res (Request Response Cycle Complete)
+
+const consoleMiddleware = (req, res, next) => {
+  console.log("This is from console middleware");
+  next();
+};
+
+const consoleMiddleware2 = (req, res, next) => {
+  console.log("This is from console middleware2");
+  throw new Error();
+  next();
+};
+
+const hamroAafnaiErrorMiddleware = (err, req, res, next) => {
+  res.send("Error Occured");
+};
+
+router.get(
+  "/login",
+  consoleMiddleware,
+  consoleMiddleware2,
+  hamroAafnaiErrorMiddleware,
+  (req, res) => {
+    res.send({
+      message: "Login Successfully",
+    });
+  }
+);
 
 export default router;
