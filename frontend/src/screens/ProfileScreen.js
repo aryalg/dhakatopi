d5953c6e9de3d7a3ebcd004cc8101d4ec1d95c89
Bikram Aryal <bikram.aryall@gmail.com>
2021-05-31 18:15:29 +0545

    Profile Screen

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { Col, Form, Row } from "react-bootstrap";
+import { Button, Col, Form, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserDetails } from "../actions/userActions";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 const ProfileScreen = ({ history }) => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("");
+  const [message, setMessage] = useState("");
 
   const userDetails = useSelector((state) => state.userDetails);
   const userLogin = useSelector((state) => state.userLogin);
@@ -29,31 +34,72 @@ const ProfileScreen = ({ history }) => {
     }
   }, [history, userInfo, user, dispatch]);
 
+  const submitHandler = (e) => {
+    console.log("Form Submitted");
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      console.log(password);
+      console.log(confirmPassword);
+      setMessage("Password are not the same");
+    } else {
+      // DISPATCH UPDATE PROFILE
+    }
+  };
+
   return (
     <Row>
       <Col md={3}>
         <h2>User Profile</h2>
+        {message && <Message variant="danger">{message}</Message>}
 
-        <Form>
+        {error && <Message variant="danger">{error}</Message>}
+        {loading && <Loader />}
+
+        <Form onSubmit={submitHandler}>
           <Form.Group controlId="name">
             <Form.Label>Name</Form.Label>
             <Form.Control
               type="name"
               paceholde="Enter Name"
               value={name}
+              onChange={(e) => setName(e.target.value)}
             ></Form.Control>
           </Form.Group>
-        </Form>
 
-        <Form>
           <Form.Group controlId="email">
             <Form.Label>Email</Form.Label>
             <Form.Control
               type="email"
               paceholde="Enter email"
               value={email}
+              onChange={(e) => setEmail(e.target.value)}
             ></Form.Control>
           </Form.Group>
+
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              paceholde="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="confirmPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              paceholde="Confirm password"
+              value={confirmPassword}
+              onChange={(e) => setconfirmPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Button type="submit" variant="primary">
+            Update
+          </Button>
         </Form>
       </Col>
     </Row>
