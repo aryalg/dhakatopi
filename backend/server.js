1b61a92da23bc26e37a926bb18b95bd41ca8caee
Bikram Aryal <bikram.aryall@gmail.com>
2021-06-10 18:26:15 +0545

    Added backend code for oder placement

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
 
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
 
@@ -32,6 +33,7 @@ app.get("/", (req, res, next) => {
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.use(notFound);
 
